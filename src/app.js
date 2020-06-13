64be5a04c6c71bd4c030f818b96afdc3f9991598
rchtnhr007@gmail.com <rchtnhr007@gmail.com>
2020-06-13 09:15:23 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs=require('hbs')
 
 const app = express()
+const port=process.env.PORT||3000
 
 const geocode=require('./utils/geocodes.js')
 const forecast=require('./utils/forecast.js')
@@ -80,6 +81,6 @@ app.get('*',(req,res)=>{
        error:'eroor 404'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 }) 
